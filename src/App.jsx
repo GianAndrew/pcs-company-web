5487b8698abb807375756dd607007a9612c825e3
Gian Del Rosario <giandrew03@gmail.com>
2023-08-31 19:00:17 +0800

    change method in router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import BaseLayout from './components/BaseLayout';
@@ -7,17 +7,17 @@ import BaseLayout from './components/BaseLayout';
 const Home = lazy(() => import('./page/Home'));
 const ErrorPage = lazy(() => import('./page/ErrorPage'));
 
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route element={<BaseLayout />}>
-			<Route path='/' element={<Home />} />
-			<Route path='*' element={<Navigate to={'/'} />} />
-		</Route>
-	)
-);
-
 const App = () => {
-	return <RouterProvider router={router} />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<BaseLayout />}>
+					<Route path='/' element={<Home />} />
+				</Route>
+				<Route path='*' element={<ErrorPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
